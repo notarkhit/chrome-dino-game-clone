e6975d0a742132fa1fc84b755df9bf71f9fae27e
Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
2022-04-21 10:58:39 +0530

    Made the ground move
    - Made helper functions to get , set and increment CSS variables

---
+++ ground.js
@@ -0,0 +1,10 @@
+import { incrementCustomProperty } from "./updateCustomProperty.js"
+
+const SPEED = .05
+const groundElems = document.querySelectorAll("[data-ground]")
+
+export function updateGround(delta) {
+  groundElems.forEach(ground => {
+    incrementCustomProperty(ground, "--left", delta * SPEED * -1)
+  })
+}
