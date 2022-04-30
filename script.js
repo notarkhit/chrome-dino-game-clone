532de8263f16d250512db78fbec3fbde46e06fd9
Aryan Arkhit Behra <notarkhit@gmail.com>
2022-04-30 12:53:21 +0530

    Added the running dino animation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { setupGround, updateGround } from './ground.js'
+import { setupDino, updateDino } from './dino.js'
 
 const WORLD_WIDTH = 100
 const WORLD_HEIGHT = 30
@@ -24,6 +25,7 @@ function update(time) {
   const delta = time - lastTime
 
   updateGround(delta ,speedScale)
+  updateDino(delta ,speedScale)
   updateSpeedScale(delta)
   updateScore(delta)
 
@@ -45,6 +47,7 @@ function handleStart() {
   speedScale = 1
   score = 0
   setupGround()
+  setupDino()
   startScreenElem.classList.add("hide")
   window.requestAnimationFrame(update)
 }
